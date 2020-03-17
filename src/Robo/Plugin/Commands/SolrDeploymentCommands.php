a125800c6e4d5f9a6c5fbea70cc55bc62f10d3cd
Jacob Sanford <jsanford@unb.ca>
2020-03-17 15:07:47 -0300

    Initial commit from template

---
+++ src/Robo/Plugin/Commands/SolrDeploymentCommands.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Dockworker\Robo\Plugin\Commands;
+
+use Dockworker\Robo\Plugin\Commands\DockworkerLocalCommands;
+
+/**
+ * Defines the commands used to interact with a local solr application.
+ */
+class SolrDeploymentCommands extends DockworkerLocalCommands {
+
+  /**
+   * Provides log checker with ignored log exception items for local Solr.
+   *
+   * @hook on-event dockworker-deployment-log-error-exceptions
+   */
+  public function getErrorLogExceptions() {
+    return [
+        'Failed to reserve shared memory' => 'Not a critical error',
+    ];
+  }
+
+}
